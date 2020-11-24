4f55ef31eacb05987405fa01cbdfd79c907713ff
Машаев Алексей Алексеевич <57888588+PewPewService@users.noreply.github.com>
2020-11-24 10:14:38 +0400

    Add files via upload

---
+++ SOA/ISerialize.cs
@@ -0,0 +1,8 @@
+﻿namespace SOA
+{
+    public interface ISerialize
+    {
+        string Serialize<T>(T obj);
+        T Deserialize<T>(string str);
+    }
+}
---
+++ SOA/JsonSerialize.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SOA
+{
+    public class JsonSerialize : ISerialize
+    {
+        public string Serialize<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T Deserialize<T> (string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+    }
+}
