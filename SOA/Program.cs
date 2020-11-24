3807bf9134677804151fed976d84665e63115a16
Машаев Алексей Алексеевич <57888588+PewPewService@users.noreply.github.com>
2020-11-24 10:15:08 +0400

    Add files via upload

---
+++ SOA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SOA
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = Console.ReadLine();
+            var obj = Console.ReadLine();
+
+            ISerialize serializer;
+            if (type.ToLower() == "xml") serializer = new XmlSerialize();
+            else serializer = new JsonSerialize();
+
+            var input = serializer.Deserialize<Input>(obj);
+            var output = new Output
+            {
+                SumResult = input.Sums.Sum() * input.K,
+                MulResult = input.Muls.Aggregate((a, b) => a * b),
+                SortedInputs = input.Sums.Concat(input.Muls.Select(n => (decimal)n)).ToArray()
+            };
+            Array.Sort(output.SortedInputs);
+
+            Console.WriteLine(serializer.Serialize(output));
+        }
+    }
+}
---
+++ SOA/XmlSerialize.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SOA
+{
+    public class XmlSerialize : ISerialize
+    {
+        public string Serialize<T>(T obj)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var sw = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sw))
+                {
+                    serializer.Serialize(writer, obj);
+                    return sw.ToString();
+                }
+            }
+        }
+
+        public T Deserialize<T>(string str)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var sr = new StringReader(str))
+            {
+                return (T)serializer.Deserialize(sr);
+            }
+        }
+    }
+}
